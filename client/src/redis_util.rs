17cd78631b57c18f0c8d575577468b534cb40df6
Nicholas LaRoche <nicholas.louis.laroche@outlook.com>
2024-06-28 23:13:29 -0600

    Cleaned up redis_utils.rs to be easier to follow.

--- client/src/redis_util.rs
+++ client/src/redis_util.rs
@@ -4,18 +4,25 @@ use std::env;
 pub async fn connect_to_redis() -> redis::Connection {
     //format - host:port
     let redis_host_name =
-        env::var("REDIS_HOSTNAME").expect("missing environment variable REDIS_HOSTNAME");
+        env::var("REDIS_HOSTNAME").expect("Missing environment variable REDIS_HOSTNAME");
     
     let redis_password = env::var("REDIS_PASSWORD").unwrap_or_default();
+    
     //if Redis server needs secure connection
     let uri_scheme = match env::var("IS_TLS") {
         Ok(_) => "rediss",
         Err(_) => "redis",
     };
-    let redis_conn_url = format!("{}://:{}@{}", uri_scheme, redis_password, redis_host_name);
+
+    let redis_conn_url = format!(
+        "{}://:{}@{}",
+        uri_scheme, redis_password, redis_host_name
+    );
+
     let client = redis::Client::open(redis_conn_url)
         .expect("Invalid connection URL")
         .get_connection()
-        .expect("failed to connect to Redis");    
+        .expect("failed to connect to Redis");
+
     client
 }
